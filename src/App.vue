5845216e1894d07d5e8d6274ae1eda266b2de7e5
Andy <andy@Andys-MacBook-Pro.local>
2022-09-05 23:39:19 -0400

    composable

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
-<script>
-export default {
-  data() {
-    return {
-      product: 'Socks'
-    }
-  },
-  created() {
-    setTimeout(() => {
-      this.product = "New Socks"
-    }, 1000)
-  }
+<script setup>
+import { ref } from 'vue'
+
+const useChangeWithDelay = function(state, newVal, delay) {
+  setTimeout(() => {
+    state.value = newVal
+  }, delay)
 }
+
+const product = ref('Socks')
+  
+useChangeWithDelay(product, 'New Socks', 1000)
+
 </script>
 
 <template>
