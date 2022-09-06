23fa31d29306f32ac4b750c144ff4972c5136954
Andy <andy@Andys-MacBook-Pro.local>
2022-09-05 22:56:09 -0400

    ref

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+
+const product = ref('Socks')
+  
+
+setTimeout(() => {
+  product.value = "New Socks"
+}, 1000)
+
+</script>
+
+<template>
+  <h1>{{ product }}</h1>
+</template>
