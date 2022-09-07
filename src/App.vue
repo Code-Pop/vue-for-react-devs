67caa42a8e74bbe86f388483e0843c953fc5e9da
Andy <andy@Andys-MacBook-Pro.local>
2022-09-07 02:26:34 -0400

    list rendering

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,13 @@ import socksGreenImage from './assets/images/socks_green.jpeg'
 const product = ref('Socks')
 const image = ref(socksGreenImage)
 const inStock = true
+  
+const details = ref(['50% cotton', '30% wool', '20% polyester'])
 
+const variants = ref([
+  { id: 2234, color: 'green' },
+  { id: 2235, color: 'blue' },
+])
 </script>
   
 <template>
@@ -19,6 +25,10 @@ const inStock = true
         <h1>{{ product }}</h1>
         <p v-if="inStock">In Stock</p>
         <p v-else>Out of Stock</p>
+        <ul>
+          <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+        <div v-for="variant in variants" key="variant.key">{{ variant.color }}</div>
       </div>
     </div>
   </div>
