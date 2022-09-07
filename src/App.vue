00644ef89074fde792e6310c2a52e9e44a1a5ff7
Andy <andy@Andys-MacBook-Pro.local>
2022-09-06 23:05:14 -0400

    image

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <script setup>
 import { ref } from 'vue'
-import socksGreenImage from '@/assets/images/socks_green.jpg'
+import socksGreenImage from './assets/images/socks_green.jpeg'
 
 const product = ref('Socks')
+const image = ref(socksGreenImage)
 
 </script>
-
+  
 <template>
+  <div class="nav-bar"></div>
   <div class="product-display">
     <div class="product-container">
-      <div class="product-image">
-        <img v-bind:src="socksGreenImage">
+      <div class="product-image">    
+        <img v-bind:src="image">
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
