19a11847cec6ecd11ac3a9c9fa9a955c13d464e6
Andy <andy@Andys-MacBook-Pro.local>
2022-09-08 15:15:08 -0400

    padding, inStock ref

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import socksGreenImage from './assets/images/socks_green.jpeg'
 
 const product = ref('Socks')
 const image = ref(socksGreenImage)
-const inStock = true
+const inStock = ref(true)
   
 const details = ref(['50% cotton', '30% wool', '20% polyester'])
 
