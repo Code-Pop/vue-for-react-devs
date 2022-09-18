f1b35b937f98ce821ed092af198c8f3f4a7f7ea6
Andy <andy@Andys-MacBook-Pro.local>
2022-09-18 19:44:32 -0400

    form base

---
+++ src/components/ReviewForm.vue
@@ -0,0 +1,31 @@
+<script>
+import { reactive } from 'vue'
+
+const review = reactive({
+  name: '',
+  content: '',
+  rating: null
+})
+</script>
+
+<template>
+  <form class="review-form">
+    <h3>Leave a review</h3>
+    <label for="name">Name:</label>
+    <input id="name">
+
+    <label for="review">Review:</label>      
+    <textarea id="review"></textarea>
+
+    <label for="rating">Rating:</label>
+    <select id="rating">
+      <option>5</option>
+      <option>4</option>
+      <option>3</option>
+      <option>2</option>
+      <option>1</option>
+    </select>
+
+    <input class="button" type="submit" value="Submit">
+  </form>
+</template>
