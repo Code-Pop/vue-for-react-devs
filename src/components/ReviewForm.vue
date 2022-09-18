109edee59d0c207fca11eab6a53de1fa5b91ae8e
Andy <andy@Andys-MacBook-Pro.local>
2022-09-18 19:52:34 -0400

    v-model

--- src/components/ReviewForm.vue
+++ src/components/ReviewForm.vue
@@ -12,13 +12,13 @@ const review = reactive({
   <form class="review-form">
     <h3>Leave a review</h3>
     <label for="name">Name:</label>
-    <input id="name">
+    <input id="name" v-model="review.name">
 
     <label for="review">Review:</label>      
-    <textarea id="review"></textarea>
+    <textarea id="review" v-model="review.content"></textarea>
 
     <label for="rating">Rating:</label>
-    <select id="rating">
+    <select id="rating" v-model.number="review.rating">
       <option>5</option>
       <option>4</option>
       <option>3</option>
