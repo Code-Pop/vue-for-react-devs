02607eb50dd1956990a89770663e13ddc7a02a1c
Andy <andy@Andys-MacBook-Pro.local>
2022-09-23 01:33:58 -0400

    standalone composable

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,7 @@
 <script setup>
-import { ref, onMounted, computed, watch } from 'vue'
+import { ref } from 'vue'
 import ProductDisplay from '@/components/ProductDisplay.vue'
-
-function useCart () {
-  const cart = ref(null)
-
-  const cartCount = computed(() => {
-    if (cart.value === null) {
-      return 0
-    }
-    else {
-      return cart.value.length
-    }
-  })
-
-  onMounted(() => {
-    fetch('http://localhost:3000/cart')
-      .then(resp => resp.json())
-      .then(data => cart.value = data.content)
-  })
-
-  watch(cart, (value, oldValue) => {
-    if(oldValue === null) return
-
-    fetch('http://localhost:3000/cart', {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        content: value
-      })
-    })
-  })
-
-  const updateCart = (id) => {
-    cart.value = cart.value.concat(id)
-  }
-
-  return { cartCount, updateCart }
-}
+import useCart from '@/composables/useCart.js'
 
 const { cartCount, updateCart } = useCart()
 const premium = ref(true)
