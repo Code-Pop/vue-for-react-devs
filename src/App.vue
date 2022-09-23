2e41a22d11cf6b929dd1d8586798551b87e21c43
Andy <andy@Andys-MacBook-Pro.local>
2022-09-22 23:21:05 -0400

    useCart

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,25 @@
 import { ref, onMounted } from 'vue'
 import ProductDisplay from '@/components/ProductDisplay.vue'
 
-const cart = ref([])
+function useCart () {
+  const cart = ref([])
+
+  onMounted(() => {
+    fetch('http://localhost:3000/cart')
+      .then(resp => resp.json())
+      .then(data => cart.value = data.content)
+  })
+
+  const updateCart = (id) => {
+    cart.value.push(id)
+  }
+
+  return { cart, updateCart }
+}
+
+const { cart, updateCart } = useCart()
 const premium = ref(true)
 
-onMounted(() => {
-  fetch('http://localhost:3000/cart')
-    .then(resp => resp.json())
-    .then(data => cart.value = data.content)
-})
-
-const updateCart = (id) => {
-  cart.value.push(id)
-}
 </script>
   
 <template>
